6bd419a8c8e161a310c3c1cfb18b9cc32691d549
ZayRexan <88442043+ZayRexan@users.noreply.github.com>
2022-05-03 02:25:57 +0700

    fix calc

--- CalculatorUI/main.js
+++ CalculatorUI/main.js
@@ -1,11 +1,19 @@
 const calc = document.querySelector('.calc');
 const result = document.querySelector('.calc__screen');
 
+const backspace = document.querySelector('.calc__btn-backspace')
 const myltiply = document.querySelector('.calc__btn-myltiply').innerText
 const division = document.querySelector('.calc__btn-division').innerText
 
 calc.addEventListener('click', function (event) {
 	if (!event.target.classList.contains('calc-inner__btn')) return;
+
+	backspace.onclick = () => {
+		result.innerText = result.innerText.slice(0, result.innerText.length - 1)
+		if (result.innerText == "") {
+			result.innerText = '0'
+		}
+	}
 
 	const value = event.target.innerText;
 
@@ -41,10 +49,6 @@ calc.addEventListener('click', function (event) {
 
 			break;
 
-		case "":
-			result.innerText = result.innerText.slice(0, result.innerText.length - 1)
-			break;
-
 		default:
 			if (result.innerText == 0) {
 				result.innerText = ''
@@ -52,6 +56,8 @@ calc.addEventListener('click', function (event) {
 			result.innerText += value;
 			console.log(value)
 			break;
+
+
 	}
 	if (result.innerText.length >= 6) {
 		result.style.fontSize = '85px'
@@ -71,21 +77,29 @@ document.addEventListener('keydown', function (event) {
 
 	console.log('keyCode:' + event.keyCode)
 
-	let rez = '';
 
 
 	if (event.keyCode == 8) {
 		result.innerText = result.innerText.slice(0, result.innerText.length - 1)
-	} else if (event.keyCode == 191) {
-		let rez = '';
-		let arr = result.innerText.split('÷')
-		rez += arr.join('/')
 	} else if (event.keyCode >= 48 && event.keyCode <= 57) {
 		if (result.innerText == 0) {
 			result.innerText = ''
 		}
 		result.innerText += event.key;
 		console.log(event.key)
+	}
+
+	if (result.innerText.length >= 6) {
+		result.style.fontSize = '85px'
+	};
+	if (result.innerText.length >= 8) {
+		result.style.fontSize = '65px'
+	}
+	if (result.innerText.length >= 11) {
+		result.style.fontSize = '47px'
+	}
+	if (result.innerText.length >= 14) {
+		result.style.fontSize = '35px'
 	}
 
 })
